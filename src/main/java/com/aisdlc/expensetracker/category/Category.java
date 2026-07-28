b16860c5d2ab9d9e7b6cc824ff677bdf7239c6b1
gauravqa180-tech <gauravqa180@gmail.com>
2026-07-28 17:23:56 +0530

    Add Category entity

---
+++ src/main/java/com/aisdlc/expensetracker/category/Category.java
@@ -0,0 +1,23 @@
+package com.aisdlc.expensetracker.category;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "categories", uniqueConstraints = {
+        @UniqueConstraint(name = "uk_category_name", columnNames = "name")
+})
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 64)
+    private String name;
+}
