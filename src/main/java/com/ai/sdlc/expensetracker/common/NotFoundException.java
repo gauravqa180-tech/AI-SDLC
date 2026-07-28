a2006e74ab49087d6d13a0930f34776684ac08f5
gauravqa180-tech <gauravqa180@gmail.com>
2026-07-28 09:16:44 +0530

    Add NotFoundException

---
+++ src/main/java/com/ai/sdlc/expensetracker/common/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.ai.sdlc.expensetracker.common;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
