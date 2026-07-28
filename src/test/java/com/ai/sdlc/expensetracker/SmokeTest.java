3bdf4f1bee7be651829c4cda2663877a7332933d
gauravqa180-tech <gauravqa180@gmail.com>
2026-07-28 09:19:03 +0530

    Add basic Spring context smoke test

---
+++ src/test/java/com/ai/sdlc/expensetracker/SmokeTest.java
@@ -0,0 +1,12 @@
+package com.ai.sdlc.expensetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmokeTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
