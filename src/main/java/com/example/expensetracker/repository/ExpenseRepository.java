6af52951ee8d774d23cff0450c78cf47df7a8de4
gauravqa180-tech <gauravqa180@gmail.com>
2026-07-28 20:43:40 +0530

    Add ExpenseRepository with Specifications support

---
+++ src/main/java/com/example/expensetracker/repository/ExpenseRepository.java
@@ -0,0 +1,8 @@
+package com.example.expensetracker.repository;
+
+import com.example.expensetracker.domain.Expense;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface ExpenseRepository extends JpaRepository<Expense, Long>, JpaSpecificationExecutor<Expense> {
+}
