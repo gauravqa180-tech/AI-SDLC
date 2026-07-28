a9195e7b3f59bfb867e5f8500c829d066cecdee1
gauravqa180-tech <gauravqa180@gmail.com>
2026-07-28 09:36:06 +0530

    Add expense category enum

---
+++ src/main/java/com/aisdlc/expensetracker/domain/ExpenseCategory.java
@@ -0,0 +1,14 @@
+package com.aisdlc.expensetracker.domain;
+
+public enum ExpenseCategory {
+    FOOD,
+    TRANSPORT,
+    UTILITIES,
+    ENTERTAINMENT,
+    SHOPPING,
+    HEALTH,
+    EDUCATION,
+    TRAVEL,
+    RENT,
+    OTHER
+}
